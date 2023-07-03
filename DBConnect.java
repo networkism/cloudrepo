ba273ce40144293716f4c44d9fecd842e3069038
Wale Omotayo <omotayoolawale0@gmail.com>
2023-07-03 10:02:09 -0700

    Newest changes It's finally done

--- DBConnect.java
+++ DBConnect.java
@@ -3,4 +3,8 @@ Welcome to Local Repo
 =======
 First commit
 Some more commits
+<<<<<<< HEAD
 >>>>>>> cca2caf (Committed with new changes)
+=======
+Mpre changes to track
+>>>>>>> 585500c (Newest changes)
